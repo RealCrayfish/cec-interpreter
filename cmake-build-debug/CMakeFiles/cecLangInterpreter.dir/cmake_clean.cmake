6ee2bf8cb093040b5bd29f01ce5c373070eeb9ea
Cray <realcrayfish896@gmail.com>
2024-10-21 11:10:33 +0100

    fixed bug

---
+++ cmake-build-debug/CMakeFiles/cecLangInterpreter.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cecLangInterpreter.dir/src/main.cpp.obj"
+  "CMakeFiles/cecLangInterpreter.dir/src/main.cpp.obj.d"
+  "cecLangInterpreter.exe"
+  "cecLangInterpreter.exe.manifest"
+  "cecLangInterpreter.pdb"
+  "libcecLangInterpreter.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cecLangInterpreter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: D:/Code/cecLangInterpreter
+# Install script for directory: C:/Users/callu/Documents/Code/cec-interpreter
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -52,6 +52,6 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-  file(WRITE "D:/Code/cecLangInterpreter/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "C:/Users/callu/Documents/Code/cec-interpreter/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
